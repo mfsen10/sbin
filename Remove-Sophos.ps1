d9b13ce95d3a35c3d3ef6cbf1311a2512dac60ec
mfsen10 <30324435+mfsen10@users.noreply.github.com>
2022-07-29 10:23:02 -0500

    clean outputs, optimize functions, code standards

--- Remove-Sophos.ps1
+++ Remove-Sophos.ps1
@@ -25,50 +25,49 @@ Write-Host "******************************" -ForegroundColor Magenta
 Write-Host "** Sophos uninstall script. **" -ForegroundColor Magenta
 Write-Host "******************************`n`n`n" -ForegroundColor Magenta
 
-Function Get-Check_Program_Installed( $programName ) 
+Function Confirm-Program_Installed( $programName ) 
     {
-        $wmi_check = (Get-WMIObject -Query "SELECT * FROM Win32_Product Where Name Like '$programName%'").name.length -gt 0
+        $wmi_check = (Get-WMIObject -Property "Name" -Class "Win32_Product" -Filter "Name LIKE '$programName%'").name.length -gt 0
         return $wmi_check;
     }
 
-Function Remove-Pkg 
+Function Remove-MSIPkg 
     {
         Param ($MarkedAppGUID)
         $ChamberFiredTstamp = get-date -format 'yyyyMMdd-HHMMssZuluK'
+        Write-Debug "    DEBUG: MSIexec nixing $markedappguid at $ChamberFiredTstamp "
         #TODO: execute uninstall
-        Start-Process "msiexec.exe" -arg "/X "$MarkedAppGUID" /qn REBOOT=SUPPRESS /norestart /L*v %windir%\Temp\Uninst$MarkedAppGUID_Log-$ChamberFiredTstamp.txt" -Wait
-        Write-Debug "    DEBUG: Merking $markedappguid at $ChamberFiredTstamp "
+        #Start-Process "msiexec.exe" -arg "/X "$MarkedAppGUID" /qn REBOOT=SUPPRESS /norestart /L*v %windir%\Temp\Uninst$MarkedAppGUID_Log-$ChamberFiredTstamp.txt" -Wait
     }
 
 #### Glorified MSI Package removal function wrapper. Most Sophos modules are MSI packages. 
-Function Remove-Sophos
+Function Invoke-MSIrmEngine
     {
         Param ($Application)
         Write-Output "    Attempting to uninstall $($Application.Name)"
             try
                 {
-                    #Uninstall-Package $Application.Name
-                    # ah ah ahhhh can't use non-native packagemgmt module...
                     $InChamberAppGUID = $Application.IdentifyingNumber
                     $InChamberAppName = $Application.Name
-                    Remove-Pkg $InChamberAppGUID
-                    $RmAttemptCounter = 1
+
+                    Remove-MSIPkg $InChamberAppGUID
                     
-                    #$Installed = Get-Package | Where-Object {$_.Name -like $Application.Name}
-                    # ah ah ahhhh can't use non-native packagemgmt module...
+                    $RmAttemptCounter = 1
                     $StillInstalled = $TRUE
-                    #$StillInstalled = (Get-WmiObject win32_product -filter {IdentifyingNumber = "$InChamberAppGUID"}).name
-                    #$StillInstalled = Get-WmiObject win32_product -filter {name like "$InChamberAppName"}
-                    $StillInstalled = Get-Check_Program_Installed $InChamberAppName
-                    Write-Output "    Confirming that $InChamberAppName ($inchamberappguid) is uninstalled via name (is-installed evaluated: $StillInstalled)"
-                    While ($StillInstalled -and $RmAttemptCounter -lt 4)
+                    Write-Output "    Confirming that $InChamberAppName ($inchamberappguid) is uninstalled"
+
+                    $StillInstalled = Confirm-Program_Installed $InChamberAppName
+                    
+                    While (!$StillInstalled -and $RmAttemptCounter -lt 4)
                         {
                             Write-Output "    $InChamberAppName was not uninstalled, trying again... ($RmAttemptCounter)" 
-                            Remove-Pkg $InChamberAppGUID
+
+                            Remove-MSIPkg $InChamberAppGUID
+                            
                             $RmAttemptCounter++
                         }
 
-                    If ($StillInstalled)
+                    If (!$StillInstalled)
                         {
                             Write-Error "`n`nERROR: Unable to uninstall $InChamberAppName after $RmAttemptCounter times"
                             exit 1;
@@ -95,74 +94,92 @@ Function Remove-SED
             "C:\Program Files\Sophos\Endpoint Defense\SEDuninstall.exe"
         #>     
         Write-Output "`nAttempting to uninstall Sophos Endpoint Defense"
-        $SEDinstalled = Get-Check_Program_Installed "Sophos Endpoint Defense"
+
+        $SEDinstalled = Confirm-Program_Installed "Sophos Endpoint Defense"
+
         if ($null -ne $SEDinstalled)
         {
             try {
+                Write-Debug "DEBUG: Would be removing SED at this time.`n"
+                # Silent flag in strings.
                 #TODO: Execute uninstall
-                #start-process "C:\Program Files\Sophos\Endpoint Defense\SEDuninstall.exe"
+                #start-process "C:\Program Files\Sophos\Endpoint Defense\SEDuninstall.exe" -arg "/silent"
                 #Write-Output "Successfully removed Sophos Endpoint Defense"
-                Write-Debug "DEBUG: Would be removing SED at this time. Maybe look at flags available to ensure silent noreboot`n"
             }
             catch {
-                Write-Error "Error: Failed to remove Sophos Endpoint Defense - attempting Zap!!!!"
-                #invoke-webrequest https://download.sophos.com/tools/SophosZap.exe -outfile "%windir%\temp\SophosZap.exe"
-                #start-process "%windir%\temp\SophosZap.exe" -arg "--confirm" -Wait
+                Write-Error "Error: Failed to remove Sophos Endpoint Defense"
+
+                Invoke-SophosZap
             }
         }else{
-            Write-Output "`nNo Sophos apps found as of $(Get-Date)"
+            Write-Output "`nNo further Sophos apps are installed as of $(Get-Date)"
             exit 0;
         }
 }
+
+Function Invoke-SophosZap
+    {
+        Write-Output "`n`nAttempting Sophos Zap!!!!"
+        #TODO: Execute Sophos Zap burn-down
+        #invoke-webrequest https://download.sophos.com/tools/SophosZap.exe -outfile "$Env:windir\temp\SophosZap.exe"
+        #start-process "$Env:windir\temp\SophosZap.exe" -arg "--confirm" -Wait
+    }
 
 
 #########################################################################################################
 #### Stop potentially disruptive service 
 Function Stop-SophosServices
     {
-        Write-Output "Attempting to halt Sophos AutoUpdate Service and set bitlocker suspension for two reboots"
+        Write-Output "Attempting to halt Sophos AutoUpdate Service."
         try
             {
                 Write-Debug "DEBUG: would be stopping $((Get-Service -Name "Sophos AutoUpdate Service").Displayname)"
+                #TODO: Execute
                 #Stop-Service -Name "Sophos AutoUpdate Service" -PassThru
             }
         catch
             {
                 Write-Error "Unable to stop the AutoUpdate Service"
             }
+        }
+Function Suspend-BitlockerEncx
+    {
         ##Suspend Bitlocker to prevent lock - we're modifying kernel modules with the AV stuff which probably will trip recovery. 
-            try{
-                Write-Debug "DEBUG: would be suspending bitlocker now"
-                #Suspend-BitLocker -MountPoint "C:" -RebootCount 2
-            }
-            catch
-            {
-                Write-Error "Unable to suspend Bitlocker"
-            }
+        Write-Output "Setting bitlocker suspension for two reboots"
+        try{
+            #TODO: Execute
+            #Suspend-BitLocker -MountPoint "C:" -RebootCount 2
+        }
+        catch
+        {
+            Write-Error "Unable to suspend Bitlocker"
+        }
     }
 
 #########################################################################################################
 #### For each ordered app, find the matching installed app via wildcard for versioned naming conventions, and execute removal.
-Function Set-OrderedSophosMSIsForUninstall
+Function Initialize-OrderedSophosMSIsForUninstall
 {
-    if($null -eq $instSophApps) 
+    Param ($installedophosAppArr)
+    if(!$null -eq $installedophosAppArr) 
         {
-        Remove-SED   
-        }else{
             $removalctr = 0;
             foreach ($NamedSophappToRm in $NamedSophAppRmOrder)
                 {
                     Write-output "`nStep $removalctr. $NamedSophappToRm to be removed"
                     $rmStepping =0;
-                    foreach($FoundSophAppBlob in $instSophApps)
+                    foreach($FoundSophAppBlob in $installedophosAppArr)
                         {
                             $NamedInstApp = $FoundSophAppBlob.name
                             #write-output "      Matches $NamedInstApp ?"
                             if ($NamedInstApp -like "$NamedSophAppToRm*")
                                 {
                                     $removalappGuid=$FoundSophAppBlob.IdentifyingNumber
-                                    Write-Output "    $NamedInstApp is installed, matches current place in ordered removal. Removing $removalappGuid ($removalctr found in $rmStepping.)" 
-                                    Remove-Sophos $FoundSophAppBlob
+                                    Write-Output "    $NamedInstApp is installed, matches current place in ordered removal. Removing $removalappGuid"
+                                    Write-Debug "($removalctr found in $rmStepping.)" 
+                                    
+                                    Invoke-MSIrmEngine $FoundSophAppBlob
+
                                 }else{
                                     Write-Debug "    $NamedSophappToRm not in slot, skipping."
                                 }
@@ -170,6 +187,8 @@ Function Set-OrderedSophosMSIsForUninstall
                         }
                     $removalctr++;
                 }
+        }else{
+            Write-Output "No Further Sophos MSI's found"
         }
 }
 #########################################################################################################
@@ -192,12 +211,20 @@ $NamedSophAppRmOrder = "Sophos Remote Management System",
 "Sophos SafeGuard Client",
 "Sophos SafeGuard Preinstall"
 
-
-Write-Output "Gathering installed Sophos Apps"
-# note we'll be removing the exe-based installer after all this MSI package removal.
-$instSophApps = Get-WmiObject win32_product -filter {name like "%Sophos%" and not name like "Sophos Endpoint Defense"}
-
+Function Get-InstalledSophosMSI
+    {
+        Write-Output "Searching for installed Sophos Apps..."
+        # note we'll be removing the exe-based installer after all this MSI package removal.
+        $instSophApps = Get-WmiObject -property "Name,IdentifyingNumber" -Class "win32_product" -Filter "Name LIKE 'Sophos%' AND NOT Name='Sophos Endpoint Defense'"
+        #Get-WMIObject -Query "SELECT Name,IdentifyingNumber FROM Win32_Product Where Name Like 'Sophos%' and not name='Sophos Endpoint Defense'"
+        return $instSophApps
+    }
 
 #begin execution
+
 Stop-SophosServices
-Set-OrderedSophosMSIsForUninstall
+Suspend-BitlockerEncx
+Write-Output "Searching for installed Sophos Apps..."
+Initialize-OrderedSophosMSIsForUninstall $(Get-InstalledSophosMSI)
+Remove-SED
+#Invoke-SophosZap
