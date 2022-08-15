6f26e842f6803d4d027d2e92bbdc1e868191779e
mfsen10 <30324435+mfsen10@users.noreply.github.com>
2022-08-15 09:25:53 -0500

    Update invoke-sophos-AV-Removal.ps1
    no zap if no safeguard

--- invoke-sophos-AV-Removal.ps1
+++ invoke-sophos-AV-Removal.ps1
@@ -380,7 +380,7 @@ Remove-SED
 # $NamedSophAppRmOrder = $NamedSafeGuardAppRmOrder
 # Write-Output "`nSearching for installed SafeGuard Apps..."
 # Initialize-OrderedSophosMSIsForUninstall $(Get-InstalledSophosMSI)
-Invoke-SophosZap
+# Invoke-SophosZap
 Stop-Transcript
 exit 0;
 #endregion execute
