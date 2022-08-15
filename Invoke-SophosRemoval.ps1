b08c0b0ad8cce45f9bf7bf3e8cd294468510d759
mfsen10 <30324435+mfsen10@users.noreply.github.com>
2022-08-15 09:22:33 -0500

    enhance sed removal condition, reduce tests

--- Invoke-SophosRemoval.ps1
+++ Invoke-SophosRemoval.ps1
@@ -100,6 +100,7 @@ Function Invoke-MSIrmEngine
                     }Else{
                         Write-Output "Successfully removed $InChamberAppName"
                         $RmAttemptCounter = 0
+                        Test-Eicar
                     }
             }catch{
                 Write-Error "Error: Failed to remove $InChamberAppName"
@@ -115,8 +116,8 @@ Function Remove-SED
             {
                 try 
                     {
-                        $seduninstexe = "C:\Program Files\Sophos\Endpoint Defense\SEDuninstall.exe"
-                        Write-Output "    Attempting to uninstall Sophos Endpoint Defense"
+                        $seduninstexe = "$Env:ProgramFiles\Sophos\Endpoint Defense\SEDuninstall.exe"
+                        Write-Output "    Attempting to uninstall $SedAppName"
                         start-process $sedUninstexe -arg "/silent" -Wait
                         $SEDrmCtr = 1
                         $SEDZombie = $TRUE
@@ -138,12 +139,14 @@ Function Remove-SED
                                 Write-Output "Successfully removed $SedAppName"
                                 $SEDrmCtr = 0
                             }
-                        Write-Output "Successfully removed Sophos Endpoint Defense"
+                        Write-Output "Successfully removed $SedAppName"
+                        Test-Eicar
                     }catch{
-                        Write-Error "Error: Failed to remove Sophos Endpoint Defense"
+                        Write-Error "Error: Failed to remove $SedAppName"
+                        Test-Eicar
                     }
             }else{
-                Write-Output "    Endpoint Defense Module not found."
+                Write-Output "    $SedAppName not found."
                 Write-Output "`nNo further Sophos apps are installed as of $(Get-Date)"
             }
     }
@@ -368,7 +371,8 @@ Invoke-EscrowBitlockerToAAD
 Write-Output "`nSearching for installed Sophos Apps..."
 Initialize-OrderedSophosMSIsForUninstall $(Get-InstalledSophosMSI)
 Remove-SED
-Test-Eicar
+#only do eicar on successful av/sed removals. might exec a few times on msi but at least we get at least one per removal but not ad infinitum on schedTask.
+#Test-Eicar
 
 $NamedSafeGuardAppRmOrder = "Sophos SafeGuard Client Configuration",
 "Sophos SafeGuard Client",
