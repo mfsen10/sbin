60f27a52ab547b08ebb05b845efa7941c8bf891f
mfsen10 <30324435+mfsen10@users.noreply.github.com>
2022-08-24 16:02:12 -0500

    add bitlocker repairs

--- Invoke-SophosRemoval.ps1
+++ Invoke-SophosRemoval.ps1
@@ -269,41 +269,65 @@ Function Stop-SophosServices
         }
     }
 
+
 Function Invoke-BitlockerEscrow ($BitlockerDrive,$BitlockerKey) 
     {
         #Escrow the key into Azure AD
         #TODO: add proxy avoidance method
         try {
-            BackupToAAD-BitLockerKeyProtector -MountPoint $BitlockerDrive -KeyProtectorId $BitlockerKey 
+            BackupToAAD-BitLockerKeyProtector -MountPoint $BitlockerDrive -KeyProtectorId $BitlockerKey -ErrorAction SilentlyContinue
             #BackupToAAD-BitLockerKeyProtector -mountpoint $Env:systemdrive -KeyProtectorID $((((Get-BitLockerVolume -mountpoint $Env:systemdrive).KeyProtector)|Where-Object {$_.KeyProtectortype -eq 'RecoveryPassword'}).keyProtectorID)
             Backup-BitLockerKeyProtector -MountPoint $BitlockerDrive -KeyProtectorId $BitlockerKey -ErrorAction SilentlyContinue
             Write-Output "`nAttempted to escrow key in Azure AD AND on-prem AD - Please verify manually!`n"
         } catch {
-            Write-Error "Azure escrow failed, exiting!"
-            Stop-Transcript
-            exit 2
+            Write-Error "Azure escrow failed!"
+            #Stop-Transcript
+            #exit 2
         }
     }
 
-Function Remove-SuperfluousRecoPasswds
+    Function Remove-SuperfluousRecoPasswds
     {
-
         $recoverykeysWithPasswds=(((Get-BitLockerVolume -mountpoint $Env:systemdrive).KeyProtector)|Where-Object {$_.KeyProtectortype -eq 'RecoveryPassword'}).keyProtectorID
         $countKeys=$recoverykeysWithPasswds.count
         Write-Output "Found $countkeys KeyProtectorID's with an associated RecoveryPassword, attempting to trim to one."
-        while ($countKeys -gt 1)
+        foreach ($keyprotectorid in $recoverykeysWithPasswds)
             {
-                foreach ($keyprotectorid in $recoverykeysWithPasswds)
+                while ($countKeys -gt 1)
                     {
                         Remove-BitLockerKeyProtector -mountpoint $env:systemdrive -KeyProtectorId $keyprotectorid
+                        $recoverykeysWithPasswds=(((Get-BitLockerVolume -mountpoint $Env:systemdrive).KeyProtector)|Where-Object {$_.KeyProtectortype -eq 'RecoveryPassword'}).keyProtectorID
+                        $countKeys=$recoverykeysWithPasswds.count
+                        Write-Output "$countkeys KeyProtectorID's with an associated RecoveryPassword remain."
                     }
-                $recoverykeysWithPasswds=(((Get-BitLockerVolume -mountpoint $Env:systemdrive).KeyProtector)|Where-Object {$_.KeyProtectortype -eq 'RecoveryPassword'}).keyProtectorID
-                $countKeys=$recoverykeysWithPasswds.count
             }
-        BackupToAAD-BitLockerKeyProtector -mountpoint $Env:systemdrive -KeyProtectorID $((((Get-BitLockerVolume -mountpoint $Env:systemdrive).KeyProtector)|Where-Object {$_.KeyProtectortype -eq 'RecoveryPassword'}).keyProtectorID)
+        #BackupToAAD-BitLockerKeyProtector -mountpoint $Env:systemdrive -KeyProtectorID $((((Get-BitLockerVolume -mountpoint $Env:systemdrive).KeyProtector)|Where-Object {$_.KeyProtectortype -eq 'RecoveryPassword'}).keyProtectorID)
     }
 
-Function Get-KeyProtectorId ($BitlockerDrive) 
+
+Function Remove-UnencryptedRecoPasswds
+    {
+        $recoverykeysWithPasswds=(((Get-BitLockerVolume -mountpoint $Env:systemdrive).KeyProtector)|Where-Object {$_.KeyProtectortype -eq 'RecoveryPassword'}).keyProtectorID
+        $countKeys=$recoverykeysWithPasswds.count
+        Write-Output "Found $countkeys KeyProtectorID's with an associated RecoveryPassword, attempting to remove."
+        $encryptstatus = (get-bitlockervolume -MountPoint $env:systemdrive).EncryptionPercentage
+        $blstatus = (get-bitlockervolume -MountPoint $env:systemdrive).VolumeStatus
+        foreach ($keyprotectorid in $recoverykeysWithPasswds)
+            {
+                if ($countKeys -eq 1 -and $encryptstatus -eq 0 -and $blstatus -eq "FullyDecrypted")
+                    {
+                        Remove-BitLockerKeyProtector -mountpoint $env:systemdrive -KeyProtectorId $keyprotectorid
+                        $recoverykeysWithPasswds=(((Get-BitLockerVolume -mountpoint $Env:systemdrive).KeyProtector)|Where-Object {$_.KeyProtectortype -eq 'RecoveryPassword'}).keyProtectorID
+                        $countKeys=$recoverykeysWithPasswds.count
+                        Write-Output "$countkeys KeyProtectorID's with an associated RecoveryPassword remain."
+                        # Stop-Transcript
+                        # exit 0;
+                    }
+            }
+        #BackupToAAD-BitLockerKeyProtector -mountpoint $Env:systemdrive -KeyProtectorID $((((Get-BitLockerVolume -mountpoint $Env:systemdrive).KeyProtector)|Where-Object {$_.KeyProtectortype -eq 'RecoveryPassword'}).keyProtectorID)
+    }
+
+Function Set-SingleRecoPasswd ($BitlockerDrive) 
     {
         $BitLockerVolume = Get-BitLockerVolume -MountPoint $BitlockerDrive
         $KeyProtector = $BitLockerVolume.KeyProtector | Where-Object { $_.KeyProtectorType -eq 'RecoveryPassword' } 
@@ -314,18 +338,37 @@ Function Get-KeyProtectorId ($BitlockerDrive)
                 Write-Warning "Too many keyprotector recovery passwords on system drive ($KeyprotectorCount total)"
                 Remove-SuperfluousRecoPasswds
         }
+        
+        $encryptstatus = (get-bitlockervolume -MountPoint $env:systemdrive).EncryptionPercentage
+        $blstatus = (get-bitlockervolume -MountPoint $env:systemdrive).VolumeStatus
+        if ($encryptstatus -eq 0 -and $blstatus -eq "FullyDecrypted")
+            {
+                Write-Output "Bitlocker is not enabled on system drive, removing recovery keys if existent"
+                Remove-UnencryptedRecoPasswds
+            }
+        
+        $BitLockerVolume = Get-BitLockerVolume -MountPoint $BitlockerDrive
         $KeyProtector = $BitLockerVolume.KeyProtector | Where-Object { $_.KeyProtectorType -eq 'RecoveryPassword' } 
         $KeyProtectorCount = $Keyprotector.count
-        if ($KeyProtectorCount -lt 1)
+        $encryptstatus = (get-bitlockervolume -MountPoint $env:systemdrive).EncryptionPercentage
+        $blstatus = (get-bitlockervolume -MountPoint $env:systemdrive).VolumeStatus
+        if ($KeyProtectorCount -lt 1 -and $encryptstatus -ne 0 -and $blstatus -ne "FullyDecrypted")
             {
-                Write-Warning "No KeyProtector found for systemdrive, creating one."
+                Write-Warning "No KeyProtector found for systemdrive, and encryption is enabled! Creating Recovery Key and PW."
                 Add-BitLockerKeyProtector -MountPoint $BitlockerDrive -RecoveryPasswordProtector
                 $KeyProtector = $BitLockerVolume.KeyProtector | Where-Object { $_.KeyProtectorType -eq 'RecoveryPassword' } 
             }
+        
+    }
+
+Function Get-KeyProtectorId ($BitlockerDrive) 
+    {
+        $BitLockerVolume = Get-BitLockerVolume -MountPoint $BitlockerDrive
         $KeyProtector = $BitLockerVolume.KeyProtector | Where-Object { $_.KeyProtectorType -eq 'RecoveryPassword' } 
         return $KeyProtector.KeyProtectorId
         #(((Get-BitLockerVolume -mountpoint $Env:systemdrive).KeyProtector)|Where-Object {$_.KeyProtectortype -eq 'RecoveryPassword'}).keyProtectorID
     }
+
 Function Test-Bitlocker ($BitlockerDrive) 
     {
         #Tests the drive for existing Bitlocker keyprotectors
@@ -339,7 +382,8 @@ Function Test-Bitlocker ($BitlockerDrive)
 Function Invoke-EscrowBitlockerToAAD
     {
         Test-Bitlocker -BitlockerDrive $DriveLetter Out-Null
-        $KeyProtectorId = Get-KeyProtectorId -BitlockerDrive $DriveLetter 
+        Set-SingleRecoPasswd ($DriveLetter)
+        $KeyProtectorId = Get-KeyProtectorId -BitlockerDrive $DriveLetter
         Invoke-BitlockerEscrow -BitlockerDrive $DriveLetter -BitlockerKey $KeyProtectorId
     }
 
@@ -376,11 +420,11 @@ Function Get-SenseStatus
         while ($sensestatus -and $sensectr -lt 4)
             {
                 Write-Warning "Attempting to force-start Defender services"
-                Set-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Services\Sense" -name "Start" -value 2
-                #Set-ItemProperty "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" -name DisableRealtimeMonitoring -value 0
-                set-service -name sense -startuptype automatic
-                Start-Service sense 
-                "sc start sense"|cmd
+                Set-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Services\Sense" -name "Start" -value 2 -ErrorAction SilentlyContinue
+                Set-Service -Name sense -StartupType Automatic -ErrorAction SilentlyContinue
+                Set-Service -name sense -status Running -ErrorAction SilentlyContinue
+                Start-Service Sense -ErrorAction SilentlyContinue
+                #"sc start sense"|cmd
                 $sensectr++
             }
         if ($sensestatus)
@@ -422,7 +466,13 @@ $Kitchen=Build-Kitchen
 Write-Output "Working from $Kitchen"
 Invoke-WriteLog ("`n`nBeginning Sophos Removal Process")
 Get-SenseStatus
-Invoke-EscrowBitlockerToAAD
+
+$tpmpresent = (Get-TPM).tpmpresent
+if ($tpmpresent)
+    {
+        Invoke-EscrowBitlockerToAAD
+    }
+
 Write-Output "`nSearching for installed Sophos Apps..."
 Initialize-OrderedSophosMSIsForUninstall $(Get-InstalledSophosMSI)
 Remove-SED
